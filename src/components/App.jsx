6eb940700a8cc1142e7338c9379e65bce224dc41
Andrij Vasylkiv <andrijvasilkiv7@gmail.com>
2023-04-11 19:05:10 +0300

    task friends without styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,18 @@
 import { ProfileCard } from "./Profile";
-import user from "../user.json"
+import user from "../user.json";
+import friends from '../friends.json'
+import { FriendList } from "./FriendList";
 
 export const App = () => {
   return (
-    <div>
-      <ProfileCard description={ user }/>
+    <div style={{
+      height: '100vh',
+      justifyContent: 'center',
+      alignItems: 'center',
+      fontSize:30,
+    }}>
+      <ProfileCard description={user} />
+      <FriendList friends={friends } />
     </div>
   );
 };
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,39 +1,40 @@
-import PropTypes  from 'prop-types'
+import PropTypes from 'prop-types';
 
+export const ProfileCard = ({
+  description: {
+    avatar,
+    username,
+    tag,
+    location,
+    stats: { followers, views, likes },
+  },
+}) => {
+  return (
+    <div className="profile">
+      <div className="description">
+        <img src={avatar} alt="User avatar" className="avatar" />
+        <p className="name">{username}</p>
+        <p className="tag">{tag}</p>
+        <p className="location">{location}</p>
+      </div>
 
-export const ProfileCard = ({ description: { avatar, username, tag, location, stats: { followers, views, likes } } }) => {
-        return (
-                <div class="profile">
-  <div class="description">
-    <img
-      src={avatar}
-      alt="User avatar"
-      class="avatar"
-    />
-    <p class="name">{username}</p>
-    <p class="tag">{tag}</p>
-    <p class="location">{location}</p>
-  </div>
-
-  <ul class="stats">
-    <li>
-      <span class="label">Followers</span>
-                <span class="quantity">{followers}</span>
-    </li>
-    <li>
-      <span class="label">Views</span>
-                <span class="quantity">{views}</span>
-    </li>
-    <li>
-      <span class="label">Likes</span>
-                <span class="quantity">{likes}</span>
-    </li>
-  </ul>
-</div>
-        )
-
-}
-
+      <ul className="stats">
+        <li>
+          <span className="label">Followers</span>
+          <span className="quantity">{followers}</span>
+        </li>
+        <li>
+          <span className="label">Views</span>
+          <span className="quantity">{views}</span>
+        </li>
+        <li>
+          <span className="label">Likes</span>
+          <span className="quantity">{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 ProfileCard.propTypes = {
   description: PropTypes.shape({
@@ -41,8 +42,10 @@ ProfileCard.propTypes = {
     username: PropTypes.string.isRequired,
     tag: PropTypes.string.isRequired,
     location: PropTypes.string.isRequired,
-    followers: PropTypes.number.isRequired,
-    views: PropTypes.number.isRequired,
-    likes: PropTypes.number.isRequired
-  }).isRequired
-}
+    // stats: {
+    //   followers: PropTypes.number.isRequired,
+    //   views: PropTypes.number.isRequired,
+    //   likes: PropTypes.number.isRequired,
+    // },
+  }).isRequired,
+};
